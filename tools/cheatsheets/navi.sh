fa28cf2e6dd840d627376a414b6248437794542d
0xfab1 <f@bi.an>
2022-05-22 19:50:50 +0200

    updates

--- tools/cheatsheets/navi.sh
+++ tools/cheatsheets/navi.sh
@@ -1,14 +1,15 @@
 sudo apt update && sudo apt -y upgrade
 
 # Install Navi
-sudo apt -y install cargo ftf
+sudo apt -y install cargo fzf
 cargo install --locked navi
 
 # Config bash
 bashrc=~/.bashrc
 if [ -f "$bashrc" ];
 then
-    echo '\n### NAVI ###' >> ~/.bashrc
+    echo '' >> ~/.bashrc
+    echo '### NAVI ###' >> ~/.bashrc
     echo 'alias navi="~/.cargo/bin/navi"' >> ~/.bashrc
     echo 'eval "\$(navi widget bash)"' >> ~/.bashrc
 else
@@ -19,16 +20,17 @@ fi
 zshrc=~/.zshrc
 if [ -f "$zshrc" ];
 then
-    echo '\n### NAVI ###' >> ~/.zshrc
+    echo '' >> ~/.zshrc
+    echo '### NAVI ###' >> ~/.zshrc
     echo 'alias navi="~/.cargo/bin/navi"' >> ~/.zshrc
     echo 'eval "\$(navi widget zsh)"' >> ~/.zshrc
 else
 	echo "$zshrc file does not exist. Skipped."
 fi
 
-# Install own cheatsheet and update on each boot
-git clone "https://github.com/FullByte/navi-cheatsheet" "$(navi info cheats-path)/FullByte__navi-cheatsheet"
-(crontab -l 2>/dev/null; echo "@reboot sleep 22 && cd \"$(navi info cheats-path)/FullByte__navi-cheatsheet\" && git pull") | crontab -
+# Install own cheatsheet and update on each boot with new shell
+exec git clone "https://github.com/FullByte/navi-cheatsheet" "$(navi info cheats-path)/FullByte__navi-cheatsheet"
+exec (crontab -l 2>/dev/null; echo "@reboot sleep 22 && cd \"$(navi info cheats-path)/FullByte__navi-cheatsheet\" && git pull") | crontab -
 
 
 
